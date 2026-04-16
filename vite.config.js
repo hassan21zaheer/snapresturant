1665cfd93c496ff472a6e2dda3b36b8625834bc0
hassan <hassan21zaheer@gmail.com>
2026-04-16 11:33:25 +0500

    solve

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+      },
+    },
+  },
+});
